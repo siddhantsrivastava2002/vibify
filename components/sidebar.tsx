f3156527495ebe66f4fd984c078f45d8e281d242
siddhantsrivastava2002 <siddhantshrivastava030@gmail.com>
2022-11-01 12:36:52 +0530

    new

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,3 +1,21 @@
+import {
+    Box,
+    List,
+    ListItem,
+    ListIcon,
+    Divider,
+    Center,
+    LinkBox,
+    LinkOverlay,
+  } from '@chakra-ui/layout'
+  import {
+    MdHome,
+    MdSearch,
+    MdLibraryMusic,
+    MdPlaylistAdd,
+    MdFavorite,
+  } from 'react-icons/md'
+
 const Sidebar =(props) => {
     
 }
